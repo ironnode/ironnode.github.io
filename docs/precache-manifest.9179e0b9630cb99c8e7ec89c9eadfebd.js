1549bf793e5135081ee67412fe9e96e09131c460
AjanthaB <ajantha19991@gmail.com>
2018-12-10 23:16:36 +0530

    added /docs folder

---
+++ docs/precache-manifest.9179e0b9630cb99c8e7ec89c9eadfebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9041de087232c5743e6",
+    "url": "/static/js/main.e9041de0.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "e9041de087232c5743e6",
+    "url": "/static/css/main.aa9e1788.chunk.css"
+  },
+  {
+    "revision": "27a0f64f4350752214f659c3c035ce5e",
+    "url": "/index.html"
+  }
+];
